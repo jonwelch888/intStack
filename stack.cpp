3783d139bfadc6b124a8bd6db5ccf7f4238599b3
jonwelch888 <jon.welch@g.austincc.edu>
2024-06-04 16:30:45 -0500

    initial commit with hello world and stub files

---
+++ stack.cpp
@@ -0,0 +1,22 @@
+#include "stack.h"
+
+Stack::Stack() : top(-1) {}
+
+void Stack::push(int value) {
+    // Implementation goes here
+}
+
+int Stack::pop() {
+    // Implementation goes here
+    return 0;
+}
+
+int Stack::peek() {
+    // Implementation goes here
+    return 0;
+}
+
+bool Stack::isEmpty() {
+    return top == -1;
+}
+
---
+++ stack.h
@@ -0,0 +1,20 @@
+#ifndef STACK_H
+#define STACK_H
+
+#define STACK_SIZE 10
+
+class Stack {
+private:
+    int arr[STACK_SIZE];
+    int top;
+
+public:
+    Stack();
+    void push(int value);
+    int pop();
+    int peek();
+    bool isEmpty();
+};
+
+#endif // STACK_H
+
