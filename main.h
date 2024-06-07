4bf2182901a5ab4572686c5a37346f3f5b6975d4
jonwelch888 <jon.welch@g.austincc.edu>
2024-06-07 03:01:58 -0500

    indentation, added comment header

--- main.h
+++ main.h
@@ -1,7 +1,16 @@
+/* ***********************************************************
+Name: Jon Welch
+Assignment: 4
+Purpose of the file: This header file includes the necessary
+headers for main.cpp.
+@note [main.h] talks to [main.cpp], & [stack.h];
+************************************************************* */
+
 #ifndef MAIN_H 
 #define MAIN_H
 
 #include "stack.h"
 #include <iostream>
 #include <stdexcept>
+
 #endif // MAIN_H
