e80bad569bdb00c32bced637c5cdb20192387de2
jonwelch888 <jon.welch@g.austincc.edu>
2024-06-06 21:52:13 -0500

    deleted some unnecessary code

--- stack.cpp
+++ stack.cpp
@@ -1,3 +1,6 @@
+
+#include "stack.h"
+
 void Stack::push(int value) {
     if (top >= STACK_SIZE - 1) {
         throw std::overflow_error("Stack 
@@ -20,23 +23,6 @@ int Stack::peek() {
 underflow");
     }
     return arr[top];
-}
-#include "stack.h"
-
-Stack::Stack() : top(-1) {}
-
-void Stack::push(int value) {
-    // Implementation goes here
-}
-
-int Stack::pop() {
-    // Implementation goes here
-    return 0;
-}
-
-int Stack::peek() {
-    // Implementation goes here
-    return 0;
 }
 
 bool Stack::isEmpty() {
