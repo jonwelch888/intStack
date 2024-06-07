1e89c745ca9807b43c488a2b67fa7f4c35c1753e
jonwelch888 <jon.welch@g.austincc.edu>
2024-06-06 21:27:00 -0500

    initial commit 2436assign4

--- main.cpp
+++ main.cpp
@@ -2,6 +2,41 @@
 #include <iostream>
 
 int main() {
+    Stack stack;
+
+    try {
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        std::cout << "Top 
+element is: " << stack.peek() 
+<< std::endl;
+
+        std::cout << "Elements: 
+";
+        while 
+(!stack.isEmpty()) {
+            std::cout << 
+stack.pop() << " ";
+        }
+        std::cout << std::endl;
+
+        // Test underflow
+        stack.pop();
+    } catch (const 
+std::exception& e) {
+        std::cerr << e.what() 
+<< std::endl;
+    }
+
+    return 0;
+}
+#include 
+"main.h"
+#include <iostream>
+
+int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
