82c0c923576d1538a1075f3c43e1d2b61c4b64fd
jonwelch888 <jon.welch@g.austincc.edu>
2024-06-07 02:42:14 -0500

    indentation, double checking, added to comments

--- stack.h
+++ stack.h
@@ -1,3 +1,8 @@
+
+
+
+
+
 #ifndef STACK_H
 #define STACK_H
 
@@ -5,17 +10,33 @@
 #include <stdexcept>
 
 
-class Stack {
+class Stack
+{
+    /*********************************************
+    This class defines a Stack of integers. It allows 
+    pushing, popping, peeking, and checking if the stack 
+    is empty.
+
+    @attrib arr : the array storing the stack elements
+    @attrib top : the index of the top element in the stack
+    *********************************************/
 private:
-    int arr[STACK_SIZE];
-    int top;
+
+int arr[STACK_SIZE];
+int top;
 
 public:
-    Stack();
-    void push(int value);
-    int pop();
-    int peek();
-    bool isEmpty();
+/**********************
+constructors/destructor
+***********************/
+Stack();
+/**********************
+Stack operations
+***********************/
+void push(int value);
+int pop();
+int peek();
+bool isEmpty();
 };
 
 #endif // STACK_H
