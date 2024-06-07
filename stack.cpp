637ba32c6303e0f3cb888674962a05d000532a73
jonwelch888 <jon.welch@g.austincc.edu>
2024-06-07 02:02:37 -0500

    indentation; double checking

--- stack.cpp
+++ stack.cpp
@@ -3,31 +3,35 @@
 
 Stack::Stack() : top(-1){}
 
-void Stack::push(int value) {
-    if (top >= STACK_SIZE - 1) {
-        throw std::overflow_error("Stack 
-overflow");
+void Stack::push(int value)
+{
+    if (top >= STACK_SIZE - 1)
+    {
+        throw std::overflow_error("Stack overflow");
     }
     arr[++top] = value;
 }
 
-int Stack::pop() {
-    if (top < 0) {
-        throw std::underflow_error("Stack 
-underflow");
+int Stack::pop()
+{
+    if (top < 0)
+    {
+        throw std::underflow_error("Stack underflow");
     }
     return arr[top--];
 }
 
-int Stack::peek() {
-    if (top < 0) {
-        throw std::underflow_error("Stack 
-underflow");
+int Stack::peek()
+{
+    if (top < 0)
+    {
+        throw std::underflow_error("Stack underflow");
     }
     return arr[top];
 }
 
-bool Stack::isEmpty() {
+bool Stack::isEmpty()
+{
     return top == -1;
 }
 
