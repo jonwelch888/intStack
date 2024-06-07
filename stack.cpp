3bf6ea1a0035877f5c2efbd848b8c622ddf0a5c5
jonwelch888 <jon.welch@g.austincc.edu>
2024-06-06 21:40:35 -0500

    iteration 1

--- stack.cpp
+++ stack.cpp
@@ -1,3 +1,26 @@
+void Stack::push(int value) {
+    if (top >= STACK_SIZE - 1) {
+        throw std::overflow_error("Stack 
+overflow");
+    }
+    arr[++top] = value;
+}
+
+int Stack::pop() {
+    if (top < 0) {
+        throw std::underflow_error("Stack 
+underflow");
+    }
+    return arr[top--];
+}
+
+int Stack::peek() {
+    if (top < 0) {
+        throw std::underflow_error("Stack 
+underflow");
+    }
+    return arr[top];
+}
 #include "stack.h"
 
 Stack::Stack() : top(-1) {}
